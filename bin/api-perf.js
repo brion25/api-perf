1e24c5621d50c485e02688038385956e28cb1e58
unknown <u34003@A276LBJX.ustr.com>
2015-06-04 15:21:07 -0500

    ->Adding main structure

---
+++ bin/api-perf.js
@@ -0,0 +1,11 @@
+var request = require('request'),
+    readConfig = require('./../lib/readConfig');
+
+function ApiPerf(){
+  var config = readConfig();
+
+  console.log(config.url);
+
+}
+
+module.exports = ApiPerf;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 #!/usr/bin/env node
+
+var ApiPerf = require('./bin/api-perf');
+
+new ApiPerf();
