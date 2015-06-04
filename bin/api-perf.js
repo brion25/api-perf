a5cf5f1985c737ef8ec660c7b7cf3f9c009dfde0
brion25 <car_25_ixco@hotmail.com>
2015-06-04 15:34:27 -0500

    ->Branch: Development, was created

--- bin/api-perf.js
+++ bin/api-perf.js
@@ -5,16 +5,6 @@ function ApiPerf(){
   var config = readConfig();
 
   console.log(config.url);
-  var req = request[config.method.toLowerCase()](config.url);
-
-  req.on('error',function(err){
-    console.log(err);
-    process.exit(1);
-  })
-
-  req.on('response',function(response){
-    console.log(response);
-  })
 }
 
 module.exports = ApiPerf;
